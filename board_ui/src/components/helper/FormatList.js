8cc600b25ec03755fe58cc7e01eb4c468e702eb6
Pavan kalyan <pavan.g2001@gmail.com>
2022-09-27 11:12:58 +0530

    popform

---
+++ board_ui/src/components/helper/FormatList.js
@@ -0,0 +1,32 @@
+
+
+export const formatList = [
+    {
+        name:"Retrospective",
+        sections:["Present","Past","Future"]
+    },
+    {
+        name:"Pros & Cons",
+        sections:["Pros","Cons"]
+    },
+    {
+        name: "One Section",
+        sections:["section 1"]
+
+    },
+    {
+        name: "Two Sections",
+        sections:["section 1" , "section 2"]
+
+    },
+    {
+        name: "Three Sections",
+        sections:["section 1" , "section 2", "section 3"]
+
+    },
+    {
+        name: "Four Sections",
+        sections:["section 1" , "section 2", "section 3", "section 4"]
+
+    },
+]
