d36f26e0d133d56979282529d41e6f19498db1ce
Aman Mahore <aman.mahore@eagleview.com>
2022-10-14 16:54:59 +0530

    Created Login Form
    Created Login Form

--- board_ui/src/App.js
+++ board_ui/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
-import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import  {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Board from "./components/Board";
 import HomePage from "./components/HomePage";
 function App() {
@@ -9,8 +8,8 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/board" element={<Board/>}/>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/board" element={<Board />} />
       </Routes>
     </Router>
   );
